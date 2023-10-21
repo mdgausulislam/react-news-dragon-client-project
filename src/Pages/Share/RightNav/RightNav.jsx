125a4626915a4d63a5270fe3667c453ec9835037
unknown <gausulislam5@gmail.com>
2023-10-21 19:36:49 +0600

    left navbar added

--- src/Pages/Share/RightNav/RightNav.jsx
+++ src/Pages/Share/RightNav/RightNav.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from 'react-icons/fa';
+import QZone from '../QZone/QZone';
+import bg from "../../../assets/bg.png"
 
 const RightNav = () => {
     return (
-        <div>
-            <h2>Right Nav</h2>
+        <div className='block'>
+            <h3>Login With</h3>
+            <Button className='mb-3' variant="outline-primary"> <FaGoogle /> Login with Google</Button>
+            <Button variant="outline-secondary"><FaGithub /> Login With Github</Button>
+            <div>
+                <h3>Find us on</h3>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebook/> Facebook</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter/> Twitter</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram/> Instagram</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <QZone></QZone>
+            <div>
+                <img src={bg} alt="" />
+            </div>
         </div>
     );
 };
