bcd8f3440402cdedf7c4416b20e32b414bd15b56
unknown <gausulislam5@gmail.com>
2023-10-21 14:58:34 +0600

    update moment js

--- src/Pages/Share/Header/Header.jsx
+++ src/Pages/Share/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import logo from '../../../assets/logo.png';
+import moment from 'moment';
 
 const Header = () => {
     return (
         <div>
-            <h3>this is header</h3>
+            <div className='text-center'>
+                <img src={logo} alt="" />
+                <p className='text-secondary'>Journalism Without Fear or Favour</p>
+                <p>{moment().format("dddd, MMMM D, YYYY")}</p>
+            </div>
         </div>
     );
 };
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -2,13 +2,27 @@ import React from 'react';
 import Header from '../Pages/Share/Header/Header';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Pages/Share/Footer/Footer';
+import { Col, Container, Row } from 'react-bootstrap';
+import LeftNav from '../Pages/Share/LeftNav/LeftNav';
+import RightNav from '../Pages/Share/RightNav/RightNav';
 
 const Main = () => {
     return (
         <div>
-            <h3>This is  main page</h3>
             <Header></Header>
-            <Outlet></Outlet>
+            <Container>
+                <Row>
+                    <Col lg={3}>
+                        <LeftNav></LeftNav>
+                    </Col>
+                    <Col lg={6}>
+                        <h2>Main content Coming</h2>
+                    </Col>
+                    <Col lg={3}>
+                        <RightNav></RightNav>
+                    </Col>
+                </Row>
+            </Container>
             <Footer></Footer>
         </div>
     );
