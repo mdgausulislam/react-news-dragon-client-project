41d6a3235f74ce3a1b3dcb49326eb0a8ba0a85af
unknown <gausulislam5@gmail.com>
2023-10-21 18:41:46 +0600

    up[date latest marquee breking news

--- src/Pages/Share/Header/Header.jsx
+++ src/Pages/Share/Header/Header.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import logo from '../../../assets/logo.png';
 import moment from 'moment';
+import { Button, Container } from 'react-bootstrap';
+import Marquee from "react-fast-marquee";
+
 
 const Header = () => {
     return (
-        <div>
+        <Container>
             <div className='text-center'>
                 <img src={logo} alt="" />
                 <p className='text-secondary'>Journalism Without Fear or Favour</p>
                 <p>{moment().format("dddd, MMMM D, YYYY")}</p>
             </div>
-        </div>
+            <div className='d-flex'>
+                <Button variant="danger">Latest</Button>
+                <Marquee className='text-danger' speed={100}>
+                    I can be a React component, multiple React components, or just some text.
+                </Marquee>
+            </div>
+        </Container>
     );
 };
 
