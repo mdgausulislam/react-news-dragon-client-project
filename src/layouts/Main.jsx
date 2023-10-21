6e176beac5dcb525ad91a55169bb66a22d0411c2
unknown <gausulislam5@gmail.com>
2023-10-21 13:16:31 +0600

    bootstrap and react router sestup and create many components and display show

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../Pages/Share/Header/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Share/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <h3>This is  main page</h3>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
