f760ea911bd4df575e547c239aa65d65e98e4580
unknown <gausulislam5@gmail.com>
2023-10-22 14:04:22 +0600

    update card header and footer rating

--- src/Pages/Home/NewsCArd/NewsCard.jsx
+++ src/Pages/Home/NewsCArd/NewsCard.jsx
@@ -1,13 +1,23 @@
+import moment from 'moment';
 import React from 'react';
 import { Card, Image } from 'react-bootstrap';
+import { FaEye, FaRegBookmark, FaRegStar, FaShareAlt, FaStar } from 'react-icons/fa';
+import Rating from 'react-rating';
 import { Link } from 'react-router-dom';
 
 const NewsCard = ({ news }) => {
-    const { _id, title, details, image_url, author } = news;
+    const { _id, title, details, image_url, author, rating, total_view } = news;
     return (
         <Card className="mb-4">
-            <Card.Header>
-                <Image src={author.img} roundedCircle />
+            <Card.Header className='d-flex align-items-center'>
+                <Image style={{ height: '40px' }} src={author?.img} roundedCircle />
+                <div className='ps-2 flex-grow-1'>
+                    <p className='mb-0'>{author?.name}</p>
+                    <p>{moment(author?.published_date).format('yyyy-MM-D')}</p>
+                </div>
+                <div>
+                    <FaRegBookmark /> <FaShareAlt />
+                </div>
             </Card.Header>
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
@@ -18,7 +28,21 @@ const NewsCard = ({ news }) => {
                     }
                 </Card.Text>
             </Card.Body>
-            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+            <Card.Footer className="text-muted d-flex">
+                <div className='flex-grow-1'>
+                    <Rating
+                        placeholderRating={rating.number}
+                        readonly
+                        emptySymbol={<FaRegStar/>}
+                        placeholderSymbol={<FaStar className='text-warning'/>}
+                        fullSymbol={<FaStar/>}
+                    />
+                    <span> {rating.number}</span>
+                </div>
+                <div>
+                    <FaEye /> {total_view}
+                </div>
+            </Card.Footer>
         </Card>
     );
 };
